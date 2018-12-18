dcd2b50e159bc8fc83ce4026467402eb47c0fdac
thuonghvd0056 <thuonghvd00564@fpt.edu.vn>
2018-12-18 10:25:35 +0700

    lan1

---
+++ BookWCFRest/BookWCFRest/BookWCFRest/BookServices.svc.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BookWCFRest
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BookServices" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select BookServices.svc or BookServices.svc.cs at the Solution Explorer and start debugging.
+    public class BookServices : IBookServices
+    {
+        static IBookRepository repository = new BookRepository();
+        public List<Book> GetBookList()
+        {
+            return repository.GetAllBooks();
+        }
+
+        public Book GetBookById(string id)
+        {
+            return repository.GetBookById(int.Parse(id));
+        }
+
+        public string AddBook(Book book)
+        {
+            Book newBook = repository.AddNewBook(book);
+            return "id =" + newBook.BookId;
+        }
+
+        public string UpdateBook(Book book)
+        {
+            bool deleted = repository.UpdateBook(book);
+            if (deleted)
+            {
+                return "Updated successfully! Book with id=" + book.BookId;
+            }
+            else
+            {
+                return "Update false!";
+            }
+        }
+
+        public string DeleteBook(string id)
+        {
+            bool deleted = repository.DeleteBook(int.Parse(id));
+            if (deleted)
+                return "Deleted successfully!";
+            else
+                return "Delete false!";
+        }
+    }
+}
